92bd409978515cc86832c2397569fb1ab67ac900
Mong <dogcat614@gmail.com>
2018-10-02 13:07:42 +0800

    Hot Module Replacement - counter

---
+++ 17-Hot Module Replacement/counter/src/components/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>The count: {{ count }}</h2>
+    <button @click="count += 10">Increment</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
