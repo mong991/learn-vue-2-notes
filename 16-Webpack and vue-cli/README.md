a92f81fcf5fbe8f1d12c18e776558731f6d55d02
Mong <dogcat614@gmail.com>
2018-10-02 12:51:41 +0800

    Webpack and vue-cli - .vue file

---
+++ 16-Webpack and vue-cli/README.md
@@ -0,0 +1,14 @@
+# Vue CLI url
+https://vue-loader.vuejs.org/guide
+
+## Install Vue CLI
+```
+npm install -g @vue/cli
+# OR
+yarn global add @vue/cli
+```
+
+## Create a new project
+```
+vue create my-app
+```
---
+++ 16-Webpack and vue-cli/my-app/src/components/Message.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="box">
+    <p>
+      <slot></slot>
+    </p>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'Message'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .box {
+    background-color: #e3e3e3;
+    padding: 18px;
+    border: 1px solid #c5c5c5;
+    margin-bottom: 10px;
+  }
+</style>
